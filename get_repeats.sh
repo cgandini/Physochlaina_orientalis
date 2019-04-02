2bf77dab0b67070611437da806cdb2acabb5012e
cgandini <gandini.carolin@gmail.com>
2019-04-02 18:02:35 -0300

    Add files via upload

--- get_repeats.sh
+++ get_repeats.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+threads=$1
+mincluster=$2 ## minimum number of sequences in a cluster to re-cluster
+getal=$3 ## Y or N
+clusterall=$4 ## Y or N
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
 if ! [ -x "$(command -v makeblastdb)" ]; then
   echo 'Error: makeblastdb is not installed.' >&2
   exit 1
@@ -56,6 +63,8 @@ cd $D
 
 cat ./chr/*.fa | seqkit seq -u -w 0 > genome.fa
 
+## Calculate repeats
+
 makeblastdb -in genome.fa -dbtype nucl -parse_seqids
 
 cd chr
@@ -87,15 +96,15 @@ seqkit subseq --bed SR_coords.sorted.bed genome.fa | perl -pe 's/\:.*//g' | perl
 
 rm SR_coords.sorted.bed
 
-## calculate clusters for all sequences
+## Calculate clusters for all sequences
 
 mkdir clusters_SR
 
 cd clusters_SR
 
-vsearch --cluster_size ../SR.fa --centroids SR_unique.fa --id 1 --iddef 4 --sizeout --strand both --uc clusters_unique.uc --sizeorder --minseqlength 20 --log vsearch.txt --clusters UN_CL_ 
+vsearch --cluster_fast ../SR.fa --threads $threads --centroids SR_unique.fa --id 1 --sizeout --strand both --uc clusters_unique.uc --sizeorder --minseqlength 20 --log vsearch_UN_CL.txt --clusters UN_CL_ 
 
-vsearch --cluster_size SR_unique.fa --centroids centroids.fa --id 0.80 --iddef 0 --sizeout --sizein --strand both --uc clusters.uc --sizeorder --minseqlength 20 --log vsearch.txt --clusters CL_
+vsearch --cluster_size SR_unique.fa --threads $threads --centroids centroids.fa --id 0.80 --sizeout --sizein --strand both --uc clusters.uc --sizeorder --minseqlength 20 --log vsearch_CL.txt --clusters CL_
 
 for f in CL*; do
 
@@ -106,22 +115,24 @@ done
 
 sort -k2,2nr CL_abundance.txt > cluster_abundance.txt
 
-awk '{if($2>=50) print $0}' cluster_abundance.txt > clusters_min50seq.txt
+awk -v mincluster=$mincluster '{if($2>=mincluster) print $0}' cluster_abundance.txt > clusters_min"${mincluster}"seq.txt
 
 while IFS= read -r line; do
 	a=( $line )
 	CL=$(echo ${a[0]})
 	seqs=$(echo ${a[1]})
-	cp "${CL}" "${CL}"_"${seqs}".fa
-done < clusters_min50seq.txt
+	cp "${CL}" cluster_"${seqs}".fa
+done < clusters_min"${mincluster}"seq.txt
 
-clusters_min50seq=$(cat clusters_min50seq.txt | wc -l)
+clusters_minseq=$(cat clusters_min"${mincluster}"seq.txt | wc -l)
 
-if [ "$clusters_min50seq" -gt 0 ]; then
+if [ "$getal" == "Y" ]; then
+
+if [ "$clusters_minseq" -gt 0 ]; then
 
 	mkdir alignments
 
-	for f in CL*.fa; do
+	for f in cluster_*.fa; do
 
 		name=$(head -1 ../genome.fa | perl -pe 's/>//g')
 		CL=$(echo $f | perl -pe 's/CL_(\d*)_(\d*)\.fa.*/$1/g')
@@ -134,11 +145,13 @@ if [ "$clusters_min50seq" -gt 0 ]; then
 
 fi
 
-#get repeat bed_file with clusters names
+fi
+
+# Get repeat bed_file with clusters names
 
 	for f in CL_*; do
 
-	grep ">" $f | perl -pe 's/>(.*);size=(\d*)/$1\t$2/g' > seqs.txt
+	grep ">" $f | perl -pe 's/>(.*);size=(\d*)/$1\t$2/g' > seq_"${f}".txt
 	CL1=$(echo $f | perl -pe 's/CL_//g')
 	size1=$(awk -v CL1=$CL1 '{if($1=="C" && $2==CL1) print $3}' clusters.uc)
 
@@ -148,24 +161,43 @@ fi
 				size2=$(echo ${a[1]})
 				CL2=$(awk -v size2=$size2 -v seq=$seq '{if($1=="C" && $3==size2 && $9==seq) print $0}' clusters_unique.uc | cut -f2)
 				cat UN_CL_"${CL2}" | perl -pe "s/>(.*)_(\d*)_(\d*);size=\d*/>\1_\2_\3_${CL1}_${size1}/g" >> SR_repeats.fa
-				cat UN_CL_"${CL2}" | grep ">" | perl -pe 's/>(.*)_(\d*)_(\d*);size=\d*/$1\t$2\t$3/g' | awk -v size1=$size1 -v CL1=$CL1 '{print $0"\t"size1"_"CL1}' | uniq >> ../SR_with_cluster.bed
-			done < seqs.txt
+				cat UN_CL_"${CL2}" | grep ">" | perl -pe 's/>(.*)_(\d*)_(\d*);size=\d*/$1\t$2\t$3/g' | awk -v size1=$size1 -v CL1=$CL1 '{print $0"\t"size1"_"CL1}' | uniq >> SR_repeats.bed
+			done < seq_"${f}".txt
 
 	done
 
-if [ "$clusters_min50seq" -gt 0 ]; then
+if [ "$clusters_minseq" -gt 0 ]; then
 
-seqkit fx2tab SR_repeats.fa | perl -pe 's/(.*_\d*_\d*_\d*)_(\d*)/$1\t$2/g' | awk -F '\t' -v OFS='\t' '{if($2>=50) print $1"_"$2,$3}' | seqkit tab2fx > clusters_min50seq.fa
+seqkit fx2tab SR_repeats.fa | perl -pe 's/(.*_\d*_\d*_\d*)_(\d*)/$1\t$2/g' | awk -F '\t' -v mincluster=$mincluster -v OFS='\t' '{if($2>=mincluster) print $1"_"$2,$3}' | seqkit tab2fx > clusters_min"${mincluster}"seq.fa
 
 fi
 
 mv clusters_unique.uc vsearch_1round.uc
 mv clusters.uc vsearch_2round.uc
-rm seqs.txt SR_repeats.fa CL* UN_CL_*
+
+rm cluster_*.fa
+
+for f in seq_*.txt; do
+
+	rm $f
+
+done 
+
+for f in CL*; do 
+
+	rm $f
+
+done 
+
+for f in UN_CL*; do 
+
+	rm $f
+
+done 
 
 cd ..
 
-##stats
+## Get repeat stats
 
 awk -v OFS='\t' '{if($6>=1000) print $0}' repeats_blast.sorted.txt  | awk -v OFS='\t' '{if($5=="minus") print $12,$1-1,$2,$1"_"$3,"LR","+\n"$13,$3-1,$4,$1"_"$3,"LR","-"; else print $12,$1-1,$2,$1"_"$3,"LR","+\n"$13,$3-1,$4,$1"_"$3,"LR","+"}' | sort -k1,1 -k2,2n | awk '{if($2>=0) print $0;}' > LR_coords.bed 
 
@@ -180,7 +212,7 @@ cat IR_coords.bed SR_coords.bed TR_coords.bed | sort -k1,1 -k2,2n | cut -f1-3 > 
 echo -e "\n######## genome length ########\n" > repeats_stats.txt
 
 echo -e "${PWD##*/}" >> repeats_stats.txt
-echo -e "chromosomes: $(grep ">" genome.fa | wc -l)" >> repeats_stats.txt
+echo -e "chromosomes/scaffolds: $(grep ">" genome.fa | wc -l)" >> repeats_stats.txt
 echo -e "total (bp):\t$(seqkit fx2tab --length genome.fa --name --only-id | cut -f 4 | awk '{sum += $1} END {print sum}')"  >> repeats_stats.txt
 
 rm genome.*
@@ -271,7 +303,7 @@ rm *.tmp
 
 done
 
-## cluster info
+## Get cluster stats
 
 SR=$(grep ">" SR.fa | wc -l | perl -pe 's/ //g')
 
@@ -294,7 +326,7 @@ cd ..
 
 done
 
-## get table 
+## Get repeat table 
 
 echo -e "species name\t#chromosomos\tlength (bp)\tTotR pairs\t# of TotR\tTotR cov (%)\tLR pairs\t# of LR\tLR cov (%)\tIR pairs\t# of IR\tIR cov (%)\tSR pairs\t# of SR\tSR cov (%)\tSR in largest cluster (%)\tSR in cluster >1000 sequences (%)\tSR in cluster >50 sequences (%)\tSR in cluster >10 sequences\tTR\tTR cov (%)\tTR n SR (%)\tSR n IR (%)\t(TR,SR,IR) n LR\t20-39 bp (TotR %)\t40-59 bp (TotR %)\t60-79 bp (TotR %)\t80-99 bp (TotR %)\t100-199 bp (TotR %)\t200-299 bp (TotR %)\t300-499 bp (TotR %)\t500-999 bp (TotR %)\t>1000 bp (TotR %)\t20-39 bp (SR %)\t40-59 bp (SR %)\t60-79 bp (SR %)\t80-99 bp (SR %)\t80-85 pident (SR %)\t85-90 pident (SR %)\t90-95 pident (SR %)\t95-100 pident (SR %)" > repeat_table.txt
 
@@ -303,4 +335,80 @@ for f in ./*/repeats_stats.txt; do
 echo -e "$(awk 'NR==4' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==5' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==6' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==10' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==11' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==13' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==17' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==18' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==20' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==24' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==25' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==27' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==31' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==32' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==34' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==122' $f | cut -f3 | perl -pe 's/.*:\s+//g')\t$(awk 'NR==124' $f | cut -f3 | perl -pe 's/.*:\s+//g')\t$(awk 'NR==128' $f | cut -f3 | perl -pe 's/.*:\s+//g')\t$(awk 'NR==130' $f | cut -f3 | perl -pe 's/.*:\s+//g')\t$(awk 'NR==38' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==40' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==52' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==54' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==56' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==61' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==64' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==67' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==70' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==73' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==76' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==79' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==82' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==85' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==91' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==94' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==97' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==100' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==106' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==109' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==112' $f | perl -pe 's/.*:\s+//g')\t$(awk 'NR==115' $f | perl -pe 's/.*:\s+//g')" >> repeat_table.txt
 
 done
+
+if [ "$clusterall" == "Y" ]; then
+
+## Calculate clusters interspecies
+
+rm -r clusterall
+
+mkdir clusterall
+
+cd clusterall
+
+cat ../*/clusters_SR/clusters_min"${mincluster}"seq.fa > all_repeats_min"${mincluster}"seqs.fa
+
+vsearch --cluster_fast all_repeats_min"${mincluster}"seqs.fa --threads $threads --centroids SR_unique.fa --id 0.80 --sizein --sizeout --strand both --uc clusters_unique.uc --sizeorder --minseqlength 20 --log vsearch.txt --clusters CL_ 
+
+mkdir combinations
+
+for f in CL*; do
+
+	seqs=$(grep "size" $f | perl -pe 's/.*size=(\d*)/$1/g' | awk '{sum += $1} END {print sum}') 
+	echo -e ""${f}"\t"${seqs}"" | sort -k2,2nr >> CL_abudance.txt
+	echo "${f%%.*}" > resume.txt
+	grep ">" $f | perl -pe 's/>(.*)_\d*_\d*_(\d*)_(\d*)\;.*/$1_$3_$2/g' | sort | uniq -c | perl -pe 's/^ *([^ ]+) +/\1\t/'  >> resume.txt
+
+	CL=$(echo "${f%%.*}")
+
+	grep ">" $f |perl -pe 's/>(.*)_\d*_\d*_(\d*)_(\d*)\;.*/$1_$3_$2/g' | sort | uniq -c | perl -pe 's/^ *([^ ]+) +/\1\t/' | awk -v CL=$CL -v OFS="\t" '{print $0,CL}' >> seqs_in_cluster.txt
+
+	grep ">" $f | perl -pe 's/>(.*)_\d*_\d*_(\d*)_(\d*)\;.*/$1_$3_$2/g' | sort | uniq -c |
+
+	awk -v CL=$CL -v OFS="\t" '
+	        {
+	                A[++c] = $2
+	        }
+	        END {
+	                for ( i = 1; i <= c; i++ )
+	                {
+	                        for ( j = 1; j <= c; j++ )
+	                        {
+	                                print A[j], A[i], CL
+	                        }
+	                }
+	        }
+	' > ./combinations/"${f%%.*}".txt
+
+	rm $f
+
+done
+
+
+for f in ./combinations/*.txt; do 
+
+         cat $f | perl -pe 's/>//g' >> combinations_all.tmp
+         cat $f | perl -pe 's/>//g' | perl -pe 's/_\d*_\d*\t/\t/g' | cut -f1-2 | awk '{if ($1!=$2) print $0}' >> combinations_interspecies.tmp
+         cat $f | perl -pe 's/>//g' | perl -pe 's/_(\d*_\d*)\t/\t$1\t/g' | cut -f1-4 | awk '{if ($1==$3) print $0}' >> combinations_intraspecies.tmp
+
+done
+
+for f in *.tmp; do
+
+sort -k1,1 $f |  awk '{ if (!seen[$0]++) print $0}' > "${f%%.*}".txt
+rm $f
+
+done
+
+perl -pe 's/_(\d*_\d*)\t/\t$1\t/g' combinations_all.txt | cut -f1-4 | awk -v OFS='\t' '{if($1==$3) print $1"_"$2,$3"_"$4,"same"; else print $1"_"$2,$3"_"$4,"other"}' |  awk '{ if (!seen[$0]++) print $0}' > combinations_all_R.txt
+
+cut -f1 combinations_all_R.txt | uniq| perl -pe 's/_(\d*)_(\d*)\n/\t$1\t$2\n/g' | sort -k1,1 -k2,2nr | perl -pe 's/\t/_/g' > levels_R.txt
+
+Rscript $SCRIPTPATH/tools/get_cluster_graph.R combinations_all_R.txt levels_R.txt
+
+cd ..
+
+fi
+
+
 
--- subset_reads.sh
+++ subset_reads.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 f=$1 #contig fasta file
-R1=$2 #reads file 1
-R2=$3 #reads file 2
+r1=$2 #reads file 1
+r2=$3 #reads file 2
 threads=$4 #number of threads to use
+readformat=$5 #A, if pair end reads are with /1 and /2, B, if not
 
 if ! [ -x "$(command -v bowtie2)" ]; then
   echo 'Error: bowtie2 is not installed.' >&2
@@ -21,10 +22,21 @@ if ! [ -x "$(command -v seqtk)" ]; then
 fi
 
 bowtie2-build-s $f "${f%%.*}" 
-bowtie2-align-s -a -p $threads --local --very-sensitive-local -q -x "${f%%.*}" -1 $R1 -2 $R2 --no-unal -S "${f%%.*}".sam 
+bowtie2-align-s -a -p $threads --local --very-sensitive-local -q -x "${f%%.*}" -1 $r1 -2 $r2 --no-unal -S "${f%%.*}".sam 
 samtools view --threads $threads -h -b -S "${f%%.*}".sam | samtools sort > "${f%%.*}".bam
-samtools view "${f%%.*}".bam | awk -F'\t' '{print $1}' | perl -ne 'print if ! $x{$_}++' > reads_mapped_titles_uniq.txt 
-seqtk subseq $R1 reads_mapped_titles_uniq.txt > "${f%%.*}"_subset_1.fq
-seqtk subseq $R2 reads_mapped_titles_uniq.txt > "${f%%.*}"_subset_2.fq
+
+
+if [ $readformat == "A" ]; then
+	samtools view "${f%%.*}".bam | awk -F'\t' '{print $1}' | perl -pe 's/\/\d$//g' | perl -ne 'print if ! $x{$_}++' > reads_mapped_titles.txt 
+	perl -pe 's/\n/\/1\n/g' reads_mapped_titles.txt > reads_mapped_titles_1.txt
+	perl -pe 's/\n/\/2\n/g' reads_mapped_titles.txt > reads_mapped_titles_2.txt
+	seqtk subseq $r1 reads_mapped_titles_1.txt > "${f%%.*}"_subset_1.fq
+	seqtk subseq $r2 reads_mapped_titles_2.txt > "${f%%.*}"_subset_2.fq
+	rm reads_mapped_titles_*.txt
+else 
+	samtools view "${f%%.*}".bam | awk -F'\t' '{print $1}' | perl -ne 'print if ! $x{$_}++' > reads_mapped_titles_uniq.txt 
+	seqtk subseq $r1 reads_mapped_titles.txt > "${f%%.*}"_subset_1.fq
+	seqtk subseq $r2 reads_mapped_titles.txt > "${f%%.*}"_subset_2.fq
+fi
 
 rm *.sam
